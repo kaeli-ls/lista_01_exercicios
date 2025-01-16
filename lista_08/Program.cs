be8372953e0c32956e2336d3c39c6abe7b064ab0
KAIKY LEANDRO SOUZA CORREIA <kaiky.lscorreia@senacsp.edu.br>
2025-01-16 15:31:55 -0300

    exercicio 08

--- lista_08/Program.cs
+++ lista_08/Program.cs
@@ -7,3 +7,17 @@ determine se ela pode votar (idade >= 16). Utilize uma condição if para exibir
 a mensagem correspondente.
 o Exemplo de Saída: "Você pode votar." ou "Você não pode votar."
 */
+
+Console.Write("Para acessar a urna, digite sua idade: ");
+int idade = int.Parse(Console.ReadLine());
+
+if (idade >= 16)
+{
+    Console.WriteLine("Acesso liberado");
+    Console.WriteLine("Você pode votar");
+}
+else
+{
+    Console.WriteLine("Acesso negado");
+    Console.WriteLine("Você não pode votar");
+}
